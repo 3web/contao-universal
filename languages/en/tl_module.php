14a9bd15b561c80a6e8d7dad397b3bb529deccf8
Daniel Herren <github@delirius.ch>
2014-04-17 12:40:12 +0200

    fix legends

---
+++ languages/en/tl_module.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   Universal
+ * @author    Daniel Herren
+ * @license   GNU/LGPL
+ * @copyright 2014
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['universal'] = array('Universal data', 'Input of universal data');
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['universal_list'] = array('Universal list', '');
+
+
+$GLOBALS['TL_LANG']['tl_module']['universal_legend'] = 'Universal options';
+$GLOBALS['TL_LANG']['tl_module']['universal_list_archive'] = 'Archiv';
+$GLOBALS['TL_LANG']['tl_module']['universal_template'] = array('Template', '');
+$GLOBALS['TL_LANG']['tl_module']['universal_per_page'] = array('Number per page (0 = no page break)', '');
+
