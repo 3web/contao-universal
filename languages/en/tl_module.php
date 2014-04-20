b37aa48bfabd1d4a4f36e9b8062ea8afe572ec5e
Daniel Herren <github@delirius.ch>
2014-04-20 20:54:15 +0200

    bugfix

---
+++ languages/en/tl_module.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   Universal
+ * @author    Daniel Herren
+ * @license   GNU/LGPL
+ * @copyright 2014
+ */
+
+
+
+
+
+$GLOBALS['TL_LANG']['tl_module']['universal_legend'] = 'Universal options';
+$GLOBALS['TL_LANG']['tl_module']['universal_list_archive'] = array('Archiv', '');
+$GLOBALS['TL_LANG']['tl_module']['universal_template'] = array('Template', '');
+$GLOBALS['TL_LANG']['tl_module']['universal_per_page'] = array('Number per page', '(0 = no page break)');
+$GLOBALS['TL_LANG']['tl_module']['universal_filter_page'] = array('Apply page filter', '');
+$GLOBALS['TL_LANG']['tl_module']['universal_iscategory'] = array('List as category', '');
+
